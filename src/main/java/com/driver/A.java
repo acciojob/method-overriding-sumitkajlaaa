9d94bae19e9c649c0631782a28741fdbed8dced9
sumit <sumitkajla47@gmail.com>
2024-07-02 17:34:14 +0530

    final commit

--- src/main/java/com/driver/A.java
+++
@@ -1,8 +0,0 @@
-package com.driver;
-
-//Task 1 make class A
-public class A {
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,12 +1,27 @@
 package com.driver;
 
 public class Main {
+    public static class A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        //Task 4 of overriding meth function
+        @Override
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
 
     public static void main(String[] args) {
         //Task 3
-        B b = new B();
+        A b = new B();
         //Task 5
-        System.out.print(b.meth());
+        String ans = b.meth();
+
+        System.out.println(ans);
     }
   
 }
